7c0a4d5681c87f2db6605378e8811e25889a9841
Silver-w <luutrongphuca2w@gmail.com>
2023-04-24 08:54:04 +0700

    first commit

---
+++ conver_yaml_to_json.py
@@ -0,0 +1,18 @@
+import ruamel.yaml as yaml
+import json
+
+if __name__ == "__main__":
+    with open("user.yaml", "r") as a:
+        user_yaml = yaml.safe_load(a)
+
+    print(user_yaml)
+    print(type(user_yaml))
+
+    user_json = json.dumps(user_yaml, default=str, indent=4)
+    print(user_json)
+    print(type(user_json))
+
+    file = open("user1.json","w")
+    file.write(user_json)
+    file.close()
+
